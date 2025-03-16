26642c5ff351adaef6816d598fd9dc9e2bd802c8
kartikey <kartikkey0414@gmail.com>
2025-03-16 18:06:20 +0530

    Get and Post request added

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/product.model');
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/api/products', async (req,res)=>{
+    try{
+
+        const products = await Product.find({});
+        res.status(200).json(products);
+
+    }catch(err){
+        res.status(500).json({"message":err.message});
+    }
+})
+
+app.post('/api/products', async (req,res)=>{
+    try{
+
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+
+    }
+    catch(err){
+        res.status(500).json({message:err.message});
+    }
+})
 
 
 
