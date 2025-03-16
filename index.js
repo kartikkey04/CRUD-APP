1a2df6e748d0753a475ed2b108ae75730cfb32a0
kartikey <kartikkey0414@gmail.com>
2025-03-16 20:52:59 +0530

    Get product using id code added

--- index.js
+++ index.js
@@ -6,11 +6,24 @@ const app = express();
 
 app.use(express.json());
 
+
+
 app.get('/api/products', async (req,res)=>{
     try{
 
         const products = await Product.find({});
         res.status(200).json(products);
+
+    }catch(err){
+        res.status(500).json({"message":err.message});
+    }
+})
+
+app.get('/api/product/:id',async (req,res)=>{
+    try{
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
 
     }catch(err){
         res.status(500).json({"message":err.message});
