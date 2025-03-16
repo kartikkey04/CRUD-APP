390223a0f977bb1b22dcd488e2a02a04a5ea3018
kartikey <kartikkey0414@gmail.com>
2025-03-16 09:46:16 +0530

    model schema created

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-app.listen(3000,()=>{
-    console.log("server is running.....")
-})
+
 
 mongoose.connect("mongodb+srv://kartikkey0414:t7I8ba2AlJUt7W7M@databsedb.4id3r.mongodb.net/Node-API?retryWrites=true&w=majority&appName=DatabseDB")
-.then(()=>{console.log("DB connected")})
+.then(()=>{console.log("DB connected");
+    app.listen(3000,()=>{
+        console.log("server is running.....")
+    })
+})
 .catch(()=>{console.log("Connection failed!!!")});
