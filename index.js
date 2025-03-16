9fcd23e04960bdbcd48e27c8b00f7e16b593f9ba
kartikey <kartikkey0414@gmail.com>
2025-03-16 23:01:35 +0530

    delete product API added

--- index.js
+++ index.js
@@ -54,6 +54,25 @@ app.put('/api/product/:id',async(req,res) => {
     }
 })
 
+// delete a product
+
+app.delete('/api/product/:id', async(req,res)=>{
+    try{
+
+        const {id} = req.params;
+
+        const product = await Product.findByIdAndDelete(id);
+
+        if(!product){
+            res.status(404).json({message:"Product not found"});
+        }
+
+        res.status(200).json({message: "Product deleted successfully"});
+
+    }catch(err){
+        res.status(500).json({"message" :err.message});
+    }
+})
 
 // Add product
 
